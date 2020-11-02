141f335018e323abbadfe384e1cfc89d88805f26
Oleh Pshyk <pshyk.oleg@gmail.com>
2020-11-02 21:54:05 +0200

    start

---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"peerDependencies": true}] */
+// import 'jquery';
+// import * as $ from 'jquery';
+// import 'Popper';
+// import Popper from 'popper.js/dist/umd/popper';
+// // import 'bootstrap';
+// import 'bootstrap/js/dist/alert';
+// import 'bootstrap/js/dist/button';
+// import 'bootstrap/js/dist/carousel';
+// import 'bootstrap/js/dist/collapse';
+// import 'bootstrap/js/dist/dropdown';
+// import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/popover';
+// import 'bootstrap/js/dist/scrollspy';
+// import 'bootstrap/js/dist/tab';
+// import 'bootstrap/js/dist/toast';
+// import 'bootstrap/js/dist/tooltip';
+// import 'bootstrap/js/dist/util';
+// import 'select2';
+// window.Popper = Popper;
+
+import hmbBtn from './hmbBtn';
+
+document.addEventListener('DOMContentLoaded', () => { 
+
+  // hmbBtn();  
+  
+});
+
+if (module.hot) {
+  module.hot.accept();
+}
